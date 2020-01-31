bfa9165bac4d204819be8b0fc63862da72a6d683
Sean <seandemesa1@yahoo.com>
2020-01-31 16:13:26 -0600

    Updates

---
+++ precache-manifest.e0b33c0ab3fde7e087958ef5a8b2e8db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7877918327d920cef2befbdf7f1ced5",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "305a99e37afd22b66fb9",
+    "url": "/myportfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "68c001ed680da87d679a",
+    "url": "/myportfolio/static/css/main.4951050d.chunk.css"
+  },
+  {
+    "revision": "305a99e37afd22b66fb9",
+    "url": "/myportfolio/static/js/2.64c5dee3.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/myportfolio/static/js/2.64c5dee3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68c001ed680da87d679a",
+    "url": "/myportfolio/static/js/main.2c94583a.chunk.js"
+  },
+  {
+    "revision": "48dec2e57e3267eb589e",
+    "url": "/myportfolio/static/js/runtime-main.805f117d.js"
+  },
+  {
+    "revision": "af90baa64f920d9b4eb14ab4c25b8db6",
+    "url": "/myportfolio/static/media/a star 2.af90baa6.png"
+  },
+  {
+    "revision": "a1634527b783efd890063cbb1ddea25d",
+    "url": "/myportfolio/static/media/citruscide logo 2.a1634527.png"
+  },
+  {
+    "revision": "90cfa8ffd4047b0fcd7cab487a5b5ad1",
+    "url": "/myportfolio/static/media/machineLearning2.90cfa8ff.png"
+  }
+]);
