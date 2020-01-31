d049820ad2ff50a76ed9591579a2326283cffb66
Sean <seandemesa1@yahoo.com>
2020-01-31 16:55:49 -0600

    Update index.js
    removed hashrouter (testing)

--- seans-app/src/index.js
+++ seans-app/src/index.js
@@ -8,9 +8,7 @@ import 'react-mdl/extra/material.js'
 import {BrowserRouter, HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
-        <HashRouter>
     <App />
-        </HashRouter>
     
     , document.getElementById('root'));
 
